14d02219cb4abeb2ef6e46690bd29f032b2b4c2d
Maulik Prajapati <maulik.prajapati@cygnetinfotech.com>
2019-03-11 20:16:49 +0530

    added api endpoint to whitelist

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,5 @@
 
-const whitelist = ['http://localhost:4200', process.env.SITE_URL];
+const whitelist = ['http://localhost:4200', 'https://onlinebankapp.herokuapp.com', process.env.SITE_URL];
 export const corsOptionsDelegate = function (req, callback) {
   let corsOptions;
   if (whitelist.indexOf(req.header('Origin')) !== -1 || (!req.header('Origin') && process.env.NODE_ENV !== 'production')) {
